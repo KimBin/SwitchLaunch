5aaa4d97bf28ff5e19369a26626c14a57306507a
maojj <mao-jiajun@163.com>
2014-05-05 22:06:20 +0800

    MOD: change launchImage

--- SwitchLaunch/ViewController.m
+++ SwitchLaunch/ViewController.m
@@ -17,13 +17,38 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self createLaunchImage];
+}
+
+- (void)createLaunchImage{
+
+    NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString* documentPath = [paths objectAtIndex:0];
+
+    NSString *imagePath = [documentPath stringByAppendingPathComponent:@"LaunchImage@2x.png"];
+    [UIImagePNGRepresentation([self getImage]) writeToFile:imagePath atomically:YES];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (UIImage *)getImage{
+    UIImage *image = [UIImage imageNamed:@"LaunchImage"];
+
+    UIGraphicsBeginImageContextWithOptions(image.size, NO, 2.0);
+
+    CGContextRef context = UIGraphicsGetCurrentContext();
+
+    CGContextSaveGState(context);
+    {
+    [image drawAtPoint:CGPointZero];
+
+    [[UIColor redColor] setFill];
+
+    CGContextFillRect(context, CGRectMake(100, 100, 200, 300));
+    }
+    CGContextRestoreGState(context);
+
+    UIImage *result = UIGraphicsGetImageFromCurrentImageContext();
+    return result;
 }
 
 @end
