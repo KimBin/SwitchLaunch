72d75c7a639eec1a34fe63dbc0a564e060af074f
maojj <mao-jiajun@163.com>
2014-05-05 21:57:35 +0800

    Initial Commit

---
+++ SwitchLaunch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SwitchLaunch
+//
+//  Created by maojj on 5/5/14.
+//  Copyright (c) 2014 maojj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SwitchLaunch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SwitchLaunch
+//
+//  Created by maojj on 5/5/14.
+//  Copyright (c) 2014 maojj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
